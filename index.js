2532461a13f5d4748359024b2e3023377f892b69
Jayshol <jayshol@gmail.com>
2018-05-29 19:12:54 -0700

    Added new search options

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const geoCodeEndpoint = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB7B_ofAzLU1alRq_vQhShyIuJZdpL_k_M";
 
+const vegFriendlyEndpoint = "https://www.vegguide.org/search/by-lat-long/";
+
+const veg_filter = "/filter/category_id=1;veg_level=4;distance=";
+
 let map;
 let zipcode;
+let latLng;
 
 function handleSubmit(){
   $('.query-form').submit((event) => {
@@ -23,64 +28,130 @@ function getLocationDetails(){
 function getCoOrdinates(data){
   if(data.status === "OK"){
     const coords =  data.results[0].geometry.location;
-    const str = 'restaurants+near+' + zipcode;
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: coords,
-      zoom: 11
-    });
+    latLng = coords;        
 
-    const queryObj = {      
-      query: str,
-      location : coords,
-      radius:10      
-    };
-     
-    const service = new google.maps.places.PlacesService(map);
-    service.textSearch(queryObj, displayRestaurantListings);
+    searchForAllRestaurants(coords);
+   // console.dir(coords);
+    /*
+    if($('#vegVegan:checked').val() === "on"){
+      searchForVegRestaurants(coords, cuisine);
+    }else{
+      searchForAllRestaurants(coords, cuisine);
+    } 
+    */    
   }
  
 }
-
-function displayRestaurantListings(dataResults, status){
+/*
+function searchForVegRestaurants(coords, cuisine){
+  // console.dir(coords);
   
-  if(status === google.maps.places.PlacesServiceStatus.OK){
-    
-    resetRestaurantListings();
-    
-    for(let i=0;i<dataResults.length;i++){
-      
-      const marker = createMarker(map, dataResults[i]);
-      
-      const infoWindow = createInfoWindow(dataResults[i]);
+  let searchEndpoint = vegFriendlyEndpoint + coords.lat + "," + coords.lng;
+  searchEndpoint += veg_filter + $('#distance').val();
+  // alert(searchEndpoint);
+  $.getJSON(searchEndpoint, getVegRestaurants);
 
-      marker.addListener('click', function(){  
-        infoWindow.open(map, marker);
-      });
-      
-      const $rowHtml = restaurantRowHTML(dataResults[i]); 
-      
-      $("#restaurantListings").append($rowHtml);
+}
 
-      $rowHtml.click(function(){   
-        infoWindow.open(map, marker);
-      });
-
+function getVegRestaurants(data){
+  resetRestaurantListings();
+  if(data.entry_count === 0){
+    noResultsHTML();
+  }else{
+    for(let i=0;data.entries.length;i++){
+      
     }
-  } 
+  }
+}  */
+
+function noResultsHTML(){
+  let result = "<li><div class='noResults'>No results found.</div></li>";
+  $('#restaurantListings').append(result);
+}
+
+function searchForAllRestaurants(coords){
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: coords,
+      zoom: 8,
+      mapTypeId: 'hybrid'
+    });
+  const openNow = $('#openNow:checked') .val();
+  const distanceInMeters = Math.floor($('#distance').val() * 1609.344);
+  const cuisine = $('#cuisine').val();
+  const vegetarian = $('#vegetarian:checked').val();
+  const vegan = $("#vegan:checked").val();
+  
+  let str = 'restaurants+near+' + zipcode;
+
+  if(cuisine !== "all"){
+    str = cuisine + "+" + str;
+  }
+  if(vegetarian === "on"){
+    str = "vegan+" + str;
+  }
+   // alert(str)   ;
+  const queryObj = {      
+    query: str,
+    location : coords,
+    radius: distanceInMeters      
+  };
+
+  if(openNow === "on"){
+    queryObj.openNow = true;   
+  }  
+    
+  const service = new google.maps.places.PlacesService(map);
+  service.textSearch(queryObj, getRestaurantListings);
+}
+
+function getRestaurantListings(dataResults, status){
+  if(dataResults.length === 0){
+    noResultsHTML();
+  }else{
+    if(status === google.maps.places.PlacesServiceStatus.OK){
+    
+      resetRestaurantListings();    
+      
+      for(let i=0;i<dataResults.length;i++){
+
+        //console.dir(dataResults[i]);
+        
+        const placeDetailsQuery = {
+          placeId: dataResults[i].place_id
+        };
+        //console.dir(placeDetailsQuery);
+  
+        const service = new google.maps.places.PlacesService(map);
+        service.getDetails(placeDetailsQuery, getPlaceDetails);  
+      }
+    } 
+  }    
 }
 
 function createMarker(map, restaurant){
   return new google.maps.Marker({
             map:map,
             title:restaurant.name,
-            position: restaurant.geometry.location
+            position: restaurant.geometry.location,
+            icon: 'restaurant.png'
           });
 }
 
 function restaurantRowHTML(restaurant){
-  return  $(`<li class="rowClass">
-              <div class="nameClass">${restaurant.name}</div>
-              <p>${restaurant.formatted_address}</p>
+  const width = restaurant.photos[0].width;
+  const height = restaurant.photos[0].height;  
+  return  $(`<li >
+              <div class="rowClass">
+              <div class="rowImage">
+                <img id='restaurantImg' src=${restaurant.photos[0].getUrl({'maxWidth':width, 'maxHeight': height})} alt='${restaurant.name}' />
+              </div>
+              <div class="nameDiv">
+                <div class="nameClass">
+                <h4>${restaurant.name}</h>
+                <h5>${restaurant.formatted_address}</h5>
+                </div>
+              </div>
+              </div>              
             </li>`);
 }
 
@@ -89,9 +160,23 @@ function resetRestaurantListings(){
 }
 
 function createInfoWindow(restaurant){
-  const contentString = `<div><h2>
-                          ${restaurant.name}</h2>
-                          <span><strong>Rating : </strong>${restaurant.rating}</span>
+  const width = restaurant.photos[0].width;
+  const height = restaurant.photos[0].height;
+  let url = restaurant.website;
+  if(url === undefined){
+    url = restaurant.url;
+  }
+ 
+  const contentString = `<div id='contentDiv'>
+                          <div id='imgDiv'>
+                            <img id='restaurantImg1' src=${restaurant.photos[0].getUrl({'maxWidth':width, 'maxHeight': height})} alt='${restaurant.name}' />
+                          </div>
+                          <div id='infoDiv'>
+                            <h2>${restaurant.name}</h2>
+                            <span><strong>Rating : </strong>${restaurant.rating}</span>
+                            <h3><strong>Phone: </strong>${restaurant.formatted_phone_number}</h3>
+                            <h3>URL :${url}</h3> 
+                          </div>
                         </div>`;
                         
   return new google.maps.InfoWindow({
@@ -99,9 +184,51 @@ function createInfoWindow(restaurant){
   });
 }
 
+function getPlaceDetails(place, status){
+ // console.dir(place);
+  if(status === google.maps.places.PlacesServiceStatus.OK){
+    const marker = createMarker(map, place);
+    
+    console.dir(place);
+    const infoWindow = createInfoWindow(place);
+
+    marker.addListener('click', function(){  
+      infoWindow.open(map, marker);
+    });
+      
+    const $rowHtml = restaurantRowHTML(place); 
+      
+    $("#restaurantListings").append($rowHtml);
+
+    $rowHtml.click(function(){   
+      infoWindow.open(map, marker);
+    });   
+  }else{
+
+  }
+  map.setCenter(place.geometry.location);
+  map.setZoom(13) ;
+}
+
+function getPosition(position){
+  const latLngObj = {
+    lat: position.coords.latitude,
+    lng:position.coords.longitude
+  }
+
+  //console.dir(latLngObj);
+  searchForAllRestaurants(latLngObj);
+}
+function error(){
+  
+}
+
 $(function(){
   handleSubmit();
   // set some default zipcode for initial search
-  zipcode = 92882;
-  getLocationDetails();
+ // zipcode = 92882;
+ // getLocationDetails();
+ if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(getPosition, error);
+ }
 });
